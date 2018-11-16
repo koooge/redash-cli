4a9886aa4d79955ef5302641f97eaef7df7caa70
koooge <koooooge@gmail.com>
2018-11-16 14:37:42 +0900

    groups: Create testcases
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ cmd/groups_test.go
@@ -0,0 +1,73 @@
+package cmd
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/koooge/redash-sdk-go/redash"
+)
+
+type groupsClient struct {
+	*redash.Client
+}
+
+const getGroupListResBody = `[{"something":"something"}]`
+
+func (c *groupsClient) GetGroupList() *redash.GetGroupListOutput {
+	return &redash.GetGroupListOutput{StatusCode: 200, Body: getGroupListResBody}
+}
+
+func TestNewCmdGetGroupList(t *testing.T) {
+	testClient := &groupsClient{}
+
+	testCases := []struct {
+		args []string
+		want string
+	}{
+		{args: []string{}, want: getGroupListResBody},
+	}
+
+	for _, c := range testCases {
+		buf := new(bytes.Buffer)
+		cmd := NewCmdGetGroupList(testClient)
+		cmd.SetOutput(buf)
+		cmd.SetArgs(c.args)
+		cmd.Execute()
+
+		get := buf.String()
+		if c.want != strings.TrimRight(get, "\n") {
+			t.Errorf("unexpected response: want:%+v, get:%+v", c.want, get)
+		}
+	}
+}
+
+const getGroupResBody = `{"something":"something"}`
+
+func (c *groupsClient) GetGroup(_ *redash.GetGroupInput) *redash.GetGroupOutput {
+	return &redash.GetGroupOutput{StatusCode: 200, Body: getGroupResBody}
+}
+
+func TestNewCmdGetGroup(t *testing.T) {
+	testClient := &groupsClient{}
+
+	testCases := []struct {
+		args []string
+		want string
+	}{
+		{args: []string{"--group-id", "1"}, want: getGroupResBody},
+	}
+
+	for _, c := range testCases {
+		buf := new(bytes.Buffer)
+		cmd := NewCmdGetGroup(testClient)
+		cmd.SetOutput(buf)
+		cmd.SetArgs(c.args)
+		cmd.Execute()
+
+		get := buf.String()
+		if c.want != strings.TrimRight(get, "\n") {
+			t.Errorf("unexpected response: want:%+v, get:%+v", c.want, get)
+		}
+	}
+}
